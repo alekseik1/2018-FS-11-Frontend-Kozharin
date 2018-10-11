f5d930b4eac46ff832bf9dd765cfb2de466ffec6
alekseik1 <1alekseik1@gmail.com>
2018-10-11 19:04:00 +0300

    DONE: сделал требуемое в дз

--- src/lib/test.js
+++ src/lib/test.js
@@ -1,5 +1,8 @@
 const say = function (name) {
-    return `Hello ${name}`;
+    document.write("<style> body {background-color: blue;}</style><div class=\"container\" id=\"div_cc\">\n" +
+        "    <h3>Меняющийся фон</h3>\n" +
+        "    <h4>Кажется, я потихоньку разбираюсь</h4>\n" +
+        "</div>")
 };
 
 export default say;
