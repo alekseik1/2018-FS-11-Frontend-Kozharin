e239abf44f4daeb6f11f8818e2627b9eacf0a115
alekseik1 <1alekseik1@gmail.com>
2018-10-12 11:12:52 +0300

    Сделал создание div через `createElement()` и `append()`.

--- src/lib/test.js
+++ src/lib/test.js
@@ -1,8 +1,21 @@
 const say = function (name) {
-    document.write("<style> body {background-color: blue;}</style><div class=\"container\" id=\"div_cc\">\n" +
-        "    <h3>Меняющийся фон</h3>\n" +
-        "    <h4>Кажется, я потихоньку разбираюсь</h4>\n" +
-        "</div>")
+    //document.write("<style> body {background-color: blue;}</style><div class=\"container\" id=\"div_cc\">\n" +
+    //    "    <h3>Меняющийся фон</h3>\n" +
+    //    "    <h4>Кажется, я потихоньку разбираюсь</h4>\n" +
+    //    "</div>")
+    let d = document.createElement("div");
+    let textNode = document.createTextNode("Hello from js!");
+    d.style.backgroundColor = "green";
+    d.appendChild(textNode);
+    document.body.appendChild(d);
+
+    d.onclick = function() {
+        if (this.style.backgroundColor === 'green') {
+            this.style.backgroundColor = 'red';
+        } else {
+            this.style.backgroundColor = 'green';
+        }
+    }
 };
 
 export default say;
