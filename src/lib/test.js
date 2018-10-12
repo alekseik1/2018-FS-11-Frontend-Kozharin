5e4d0204cfc1e0a5f4a04f9d1ba71ba445cf0655
alekseik1 <1alekseik1@gmail.com>
2018-10-12 13:01:44 +0300

    Удалил код в комментариях

--- src/lib/test.js
+++ src/lib/test.js
@@ -1,8 +1,4 @@
 const say = function (name) {
-    //document.write("<style> body {background-color: blue;}</style><div class=\"container\" id=\"div_cc\">\n" +
-    //    "    <h3>Меняющийся фон</h3>\n" +
-    //    "    <h4>Кажется, я потихоньку разбираюсь</h4>\n" +
-    //    "</div>")
     let d = document.createElement("div");
     let textNode = document.createTextNode("Hello from js!");
     d.style.backgroundColor = "green";
