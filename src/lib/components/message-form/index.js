b84cf9009cf0ee709345bde75b898bac914051f9
alekseik1 <1alekseik1@gmail.com>
2018-11-06 00:42:32 +0300

    BETA: сделал каркас для FileSystem API

--- src/lib/components/message-form/index.js
+++ src/lib/components/message-form/index.js
@@ -63,8 +63,23 @@ class MessageForm extends HTMLElement {
             console.log(main_form);
             var files = this.files;
             for(var file of files) {
-                const messageDiv = MessageForm.createMessageDiv({text:
-                        `File: ${file.name}; createdAt: ${(new Date(file.lastModified)).toLocaleDateString()}`}, new Date());
+                var ValidImageTypes = ["image/gif", "image/jpeg", "image/png"];
+                var fileType = file['type'];
+                var messageDiv;
+                if(ValidImageTypes.includes(fileType)) {
+                    // Это картинка!!
+                    MessageForm.saveImage(file);
+                    messageDiv = MessageForm.createMessageDiv({
+                       text: `Image; created at: ${(new Date(file.lastModified)).toLocaleDateString()}`,
+                       image: file
+                    }, new Date());
+                } else {
+                    // Это не картинка
+                    messageDiv = MessageForm.createMessageDiv({
+                        text:
+                            `File: ${file.name}; createdAt: ${(new Date(file.lastModified)).toLocaleDateString()}`
+                    }, new Date());
+                }
                 var result_div = main_form.querySelector('.result');
                 result_div.appendChild(messageDiv);
             }
@@ -112,10 +127,46 @@ class MessageForm extends HTMLElement {
         else
             messageDiv.className = "other_messages";
         messageDiv.innerText = messageContent.text;
+        // Добавляем картинку, если она есть
+        if (messageContent.hasOwnProperty('image')) {
+            const imageElement = document.createElement('img');
+            imageElement.src = MessageForm.getImageSrc();
+            messageDiv.appendChild(imageElement);
+        }
         messageDiv.insertAdjacentHTML('beforeend',
             `<div class="sent_time"> ${date.getHours()}:${date.getMinutes()}</div>`);
         parentDiv.appendChild(messageDiv);
         return parentDiv;
+    }
+
+    static getImageSrc() {
+        return `https://mipt.ru/css/images/eye.png`;
+    }
+
+    static saveImage(file) {
+        window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+
+        window.requestFileSystem(window.TEMPORARY, 1024*1024*20,
+            (fs) => {
+                fs.root.getFile('pic.png', {}, function(fileEntry) {
+
+                    // Get a File object representing the file,
+                    // then use FileReader to read its contents.
+                    fileEntry.file(function(file) {
+                        var reader = new FileReader();
+
+                        reader.onloadend = function(e) {
+                            var txtArea = document.createElement('textarea');
+                            txtArea.value = this.result;
+                            document.body.appendChild(txtArea);
+                        };
+
+                        reader.readAsText(file);
+                    }, errorHandler);
+
+                }, errorHandler);
+            },
+            () => alert('Пошел ты нахер, козел!'));
     }
 
     _onSubmit (event) {
