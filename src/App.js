a755d8385677a00e881b6638015b280b30d747d0
alekseik1 <1alekseik1@gmail.com>
2018-11-20 22:31:36 +0300

    Контейнер для сообщений

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Input from './components/input/Input';
+import MessageContainer from './components/containers/message-container/MessageContainer';
 
 class App extends Component {
   render() {
     return (
       <div>
+          <MessageContainer/>
           <Input />
       </div>
     );
---
+++ src/components/containers/message-container/MessageContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './styles.css';
+
+class MessageContainer extends React.Component {
+    render() {
+        return (
+            <div className={styles.MessageContainer}>
+            </div>
+        );
+    }
+}
+
+export default MessageContainer;
