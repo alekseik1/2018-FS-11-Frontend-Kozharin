d4e2be0042e13da6b59fcd0f345cd03f92cc1c54
alekseik1 <1alekseik1@gmail.com>
2018-11-21 01:58:01 +0300

    - Правильное заполнение тестовыми сообщениями - Начало дизайна <App/>

--- src/App.js
+++ src/App.js
@@ -1,39 +1,34 @@
 import React, { Component } from 'react';
-import './App.css';
+import styles from './App.css';
 import Input from './components/input/Input';
 import MessageContainer from './components/containers/message-container/MessageContainer';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this._fill_debug_messages();
+    this.state = {messages: []};
+  }
+
+  componentDidMount() {
+      this._fill_debug_messages();
   }
 
   _fill_debug_messages() {
-      this.state = {messages: [
-              {
-                  text: 'СЛОЖНА',
-                  time: new Date().toLocaleDateString(),
-                  isRead: true,
-                  files: []
-              }
-          ]};
       let mes = [];
       for(let i=0; i<100; i++) {
-          this.state.messages.push(
-              {
-                  text: 'СЛОЖНА',
-                  time: new Date().toLocaleDateString(),
-                  isRead: true,
-                  files: []
-              }
-          );
+          mes.push({
+              text: 'СЛОЖНА',
+              time: new Date().toLocaleDateString(),
+              isRead: true,
+              files: []
+          });
       }
+      this.setState({messages: mes});
   }
 
   render() {
     return (
-      <div>
+      <div className={styles.react_container}>
           <MessageContainer messages={this.state.messages}/>
           <Input />
       </div>
