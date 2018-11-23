c5c00f172493c3e70af9c330dc5ffa563759d455
alekseik1 <1alekseik1@gmail.com>
2018-11-23 13:51:26 +0300

    FIX: форма ввода уезжала вниз.

--- src/components/containers/message-container/MessageContainer.js
+++ src/components/containers/message-container/MessageContainer.js
@@ -5,16 +5,20 @@ import Message from "../Message/Message";
 class MessageContainer extends React.Component {
 
     render() {
+        /* BUG: flex при использовании justify-content: flex-end
+        ломает скроллинг. Решение: засунуть в еще один контейнер! */
         return (
-            <div className={styles.MessageContainer}>
-                {this.props.messages.map((element, index) => {
-                    return <Message
-                        isMine={index % 2 === 0}
-                        text={element.text}
-                        time={element.time}
-                        key={index}
-                        isRead={element.isRead}/>;
-                })}
+            <div className={styles.InnerScroll}>
+                <div className={styles.MessageContainer}>
+                    {this.props.messages.map((element, index) => {
+                        return <Message
+                            isMine={index % 2 === 0}
+                            text={element.text}
+                            time={element.time}
+                            key={index}
+                            isRead={element.isRead}/>;
+                    })}
+                </div>
             </div>
         );
     }
