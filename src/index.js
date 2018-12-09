9887cd3791b817adaa1b65b7f5013fb9d653828c
alekseik1 <1alekseik1@gmail.com>
2018-12-10 01:28:51 +0300

    Настроить middleware в основном файле

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from "redux";
+import {applyMiddleware, createStore} from "redux";
 import rootReducer from './reducers';
+import thunkMiddleware from "redux-thunk";
+import { createLogger} from "redux-logger";
 
-const store = createStore(rootReducer);
+const loggerMiddleware = createLogger();
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, loggerMiddleware));
 
 ReactDOM.render(
     <Provider store={store} >
