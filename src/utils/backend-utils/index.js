2ef6f906841958dd5602334fb4df2d9482955126
alekseik1 <1alekseik1@gmail.com>
2018-12-10 02:00:02 +0300

    REF: организовать повторяющийся код в  backend-utils

--- src/utils/backend-utils/index.js
+++ src/utils/backend-utils/index.js
@@ -1,74 +1,54 @@
 // TODO: поменять на реальный сервер в продакшне
 const BACKEND_URL = 'http://localhost:5000/api/';
 
-/**
- * Загружает диалоги пользователя
- * @param userID id пользователя
- */
-function getDialogs(userID) {
-    fetch()
-}
+let currentID = 0;
 
-function getUserInfo(userID) {
+const makeJsonrpcRequest = (methodName, params) => {
+    currentID++;
+    // Вряд ли у нас будет 1000 запросов одновременно
+    currentID = Math.floor(currentID / 1000);
     return fetch(BACKEND_URL, {
         method: 'POST',
         body: JSON.stringify({
             'jsonrpc': '2.0',
-            'id': '10',
-            'method': 'get_user_info',
-            'params': {
-                'user_id': userID,
-            }
-        }),
+            'id': currentID,
+            'method': methodName,
+            'params': params
+        })
     }).then(response => response.json());
+};
+
+/**
+ * Загружает диалоги пользователя
+ * @param userID id пользователя
+ */
+function getChats(userID, token) {
+    return makeJsonrpcRequest('get_user_chats', {'user_id': userID} )
+}
+
+function getUserInfo(userID) {
+    return makeJsonrpcRequest('get_user_info', {'user_id': userID} );
 }
 
 
 function getChatMessages(chatId, userID, limit) {
     console.log('getChatMessages!');
     let messages = [];
-    return fetch(BACKEND_URL, {
-        method: 'POST',
-        body: JSON.stringify({
-            'jsonrpc': '2.0',
-            'id': '2',
-            'method': 'get_messages_by_chat',
-            'params': {
-                'chat_id': chatId,
-                'from_id': userID,
-                'limit': limit,
-            }
-        })
-    }).then(response => response.json()
+    return makeJsonrpcRequest(
+        'get_messages_by_chat',
+        {'chat_id': chatId, 'from_id': userID, 'limit': limit}
     ).then(success => {
-        console.log('Success in getChatMessages: ');
-        console.log(success);
-        if (success.length === 0) {
-            // Если не пришло сообщений, возвращаем пустой список
-            return [];
-        }
-        return success.result
-            .slice()
-            // С бека они приходят в обратном порядке (особенность SQL)
-            .reverse();
-    }).catch(err => console.log(err));
+        // С бека они приходят в обратном порядке (особенность SQL)
+        return ( (success.length === 0) ? [] : success.result.slice().reverse() );
+    });
 }
 
 // TODO: передавать файлы в сообщении
 function sendChatMessage(chatId, senderID, messageText) {
-    return fetch(BACKEND_URL, {
-        method: 'POST',
-        body: JSON.stringify({
-            'jsonrpc': '2.0',
-            'id': '3',
-            'method': 'send_message',
-            'params': {
-                'chat_id': chatId,
-                'user_id': senderID,
-                'text': messageText,
-            }
-        })
-    });
+    return makeJsonrpcRequest(
+        'send_message',
+        {'chat_id': chatId, 'user_id': senderID, 'text': messageText}
+    );
 }
 
-export {getChatMessages, getDialogs, sendChatMessage, getUserInfo};
+export {getChatMessages, getChats, sendChatMessage, getUserInfo};
