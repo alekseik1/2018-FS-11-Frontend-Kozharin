b62d0867ee40aa173e218fbaa0f399553fea193d
alekseik1 <1alekseik1@gmail.com>
2018-12-09 20:43:25 +0300

    Не сохранять submitListener в FileInput

--- src/components/input/file-input/FileInput.js
+++ src/components/input/file-input/FileInput.js
@@ -3,18 +3,10 @@ import styles from './styles.css';
 
 
 class FileInput extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            submitListener: props.submitListener,
-        }
-    }
 
     _onChange(e) {
         let files = e.target.files;
-        console.log(e);
-        console.log(files);
-        this.state.submitListener(files);
+        this.props.submitListener(files);
     }
 
     _handleClick(e) {
