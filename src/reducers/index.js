2f35245b1632e373675db37f54d72b35c7e2412c
alekseik1 <1alekseik1@gmail.com>
2018-12-09 22:13:24 +0300

    Удалить ненужные reducers. Добавить reducer для unfini

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,46 +1,39 @@
 import { combineReducers } from "redux";
-import {LOAD_CHATS, LOAD_MESSAGES, LOGIN_USER, SEND_MESSAGE} from '../actions/index';
-import {getChatMessages, getChats, getDialogs, sendChatMessage} from "../utils/backend-utils";
+import {
+    FILES_SUBMITTED, GEO_SUBMITTED,
+    TEXT_SUBMITTED
+} from '../actions/index';
 
-function loadChats(state, action) {
-    switch (action.type) {
-        case LOAD_CHATS:
-            getDialogs(action.userID);
-            return state;
-        default:
-            return state;
-    }
-}
-
-function loadMessages(state, action) {
+function unfinishedMessages(state, action) {
     switch(action.type) {
-        case LOAD_MESSAGES:
-            getChatMessages(action.chatID, action.userID, action.limit);
-            return state;
+        case TEXT_SUBMITTED:
+            return state.map((message) => {
+                // Для данного чата
+                if (message.chatID === action.chatID) {
+                    // Записываем текущий текст
+                    return {...message, text: action.text};
+                }
+                return message;
+            });
+        case FILES_SUBMITTED:
+            return state.map((message) => {
+                if (message.chatID === action.chatID) {
+                    return {...message, files: action.files}
+                }
+                return message;
+            });
+        case GEO_SUBMITTED:
+            return state.map((message) => {
+                if (message.chatID === action.chatID) {
+                    return {...message, geo: action.geo}
+                }
+                return message;
+            });
         default:
             return state;
     }
 }
-
-// TODO: не используется. Здесь будет авторизация/запрос на авторизацию/пока не придумал.
-function loginUser(state, action) {
-    return state;
-}
-
-function sendMessage(state, action) {
-    switch(action.type) {
-        case SEND_MESSAGE:
-            sendChatMessage(action.chatID, state.userID,
-                action.text, action.attachment);
-            return state;
-        default:
-            return state;
-    }
-}
-
 
 export default combineReducers({
-    loadChats,
-    loadMessages,
-    loginUser
+    unfinishedMessages,
 });
