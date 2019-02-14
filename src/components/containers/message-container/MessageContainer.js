0783c53bb6fa5f1470b727b2dbab8ec6ddd97d1e
alekseik1 <alekseik1@yandex.ru>
2019-02-14 11:44:20 +0300

    feat: добавить валидацию схемы `message`

--- src/components/containers/message-container/MessageContainer.js
+++ src/components/containers/message-container/MessageContainer.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom'
 import styles from './styles.css';
 import Message from "../Message/Message";
 
+const schema = require("schm");
+const messageSchema = schema({
+    isMine: true,
+    text: "Hello, message",
+    time: new Date().toLocaleString(),
+    isRead: false,
+});
+
 class MessageContainer extends React.Component {
     constructor(props) {
         super(props);
@@ -30,6 +38,7 @@ class MessageContainer extends React.Component {
             <div className={styles.InnerScroll}>
                 <div className={styles.MessageContainer}>
                     {this.props.messages.map((element, index) => {
+                        console.log('validating message: ', messageSchema.validate(element) ? 'OK': 'BAD!');
                         return <Message
                             isMine={element.isOwn}
                             text={element.text}
